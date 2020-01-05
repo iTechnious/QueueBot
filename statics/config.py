1e7d15ad9ef62365f5c58912efdb246d52616124
AuxiliumCDNG <soenke.klock@pm.me>
2020-01-05 02:11:29 +0100

    Update config.py

--- statics/config.py
+++ statics/config.py
@@ -1,3 +1,11 @@
 bot = {
-    "token": "NTY3MDMzNDA4Mjk1Nzk2NzY3.XLN-kQ.S2Go6jZUd3OqXKEYlc2Hv4Pvsco"
+    "token": "NTY3MDMzNDA4Mjk1Nzk2NzY3.XhEqdg.dZ4r-oP4LQ8bN4YEZnfk1MbcLko"
+}
+
+mysql = {
+    "host": "itechnious.com",
+    "port": 3306,
+    "user": "queue",
+    "pass": "mZlDROiKPaUD8YQX",
+    "db": "queue"
 }
