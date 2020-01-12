695a735611bdc918315ae2681418f1221e9b9ec4
AuxiliumCDNG <47604750+AuxiliumCDNG@users.noreply.github.com>
2020-01-12 20:00:43 +0100

    Create sample_config.py

---
+++ statics/sample_config.py
@@ -0,0 +1,13 @@
+# rename this to "config.py"
+
+bot = {
+    "token": "" # enter your bot token from discord here
+}
+
+mysql = { # enter your mysql config data here
+    "host": "",
+    "port": 0000,
+    "user": "",
+    "pass": "",
+    "db": ""
+}
