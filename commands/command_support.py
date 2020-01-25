a0007eb07176309fdee687a56ad57d321efc486a
AuxiliumCDNG <soenke.klock@pm.me>
2020-01-25 22:17:01 +0100

    support feature
    -music loop
    -ticket system
    -config entrys for support system

--- commands/command_support.py
+++ commands/command_support.py
@@ -15,18 +15,46 @@ async def execute(client, message, args):
             await message.channel.send("You don't have permission to do that.\nAsk for the **Queue Admin** role.")
             return
 
-    # check if correct role id was provided
+    # check if correct option was provided
     if args == []:
-        await message.channel.send("Please provide the role id of the support team.")
+        await message.channel.send("Please provide the role id of the support team.\n"
+                                   "You can also set the support text channel by typing 'text' before the channel id.")
+        return
+    
+    if args[0] == "text":
+        try:
+            args[1] = int(args[1])
+        except ValueError:
+            await message.channel.send("Please Provide a channel ID. You can get the role ID by right clicking on the channel.\n"
+            "Please make sure to enable developer mode first.")
+            return
+        if get(message.guild.text_channels, id=args[1]):
+            if change_setting(message.guild.id, "supporttext", str(args[1])):
+                await message.channel.send("Text Channel changed.")
+            else:
+                await message.channel.send("There was an error changing the Support Channel. It's probably not your fault. Please consult the bot hoster!")
+
+        else:
+            await message.channel.send("This channel doesn't exist on this server or is not a text channel.")
+            return
         return
 
-    if not get(message.guild.roles, id=int(args[0])):
-        await message.channel.send("The role provided doesn't exist on this server.")
-        return
-
-    if change_setting(message.guild.id, "supportrole", str(args[0])):
-        await message.channel.send("Support Role changed.")
     else:
-        await message.channel.send("There was an error changing the Support Channel. It's probably not your fault. Please consult the bot hoster!")        
+        try:
+            args[0] = int(args[0])
+        except ValueError:
+            await message.channel.send("Please Provide a role ID. You can get the role ID by right clicking on the role in your server settings.\n"
+            "Please make sure to enable developer mode first.")
+            return
 
-    return
+        if not get(message.guild.roles, id=args[0]):
+            await message.channel.send("The role provided doesn't exist on this server.")
+            return
+
+        # change and check change; notify on discord
+        if change_setting(message.guild.id, "supportrole", str(args[0])):
+            await message.channel.send("Support Role changed.")
+        else:
+            await message.channel.send("There was an error changing the Support role. It's probably not your fault. Please consult the bot hoster!")        
+
+        return
--- main.py
+++ main.py
@@ -7,8 +7,10 @@ import time
 import random
 import warnings
 from shutil import copy
+import youtube_dl
 
 import discord
+from discord import FFmpegPCMAudio
 from discord.utils import get
 import pymysql
 
@@ -67,31 +69,22 @@ class client_class(discord.Client):
         print(list_commands)
 
 
-    async def on_voice_state_update(self, member, before, after):
+    async def on_voice_state_update(self, member, before, after):        
+        if member.id == client.user.id:
+            return
         supportchannel = get_setting(str(member.guild.id), "supportchannel")
+        channel = member.guild.get_channel(int(supportchannel))
         supportrole = get_setting(str(member.guild.id), "supportrole")
+        voice = get(client.voice_clients, guild=member.guild)
 
-        if after.channel is None:
-            if str(before.channel.id) == str(supportchannel):
-                del tickets[member.id]
-                print(member.name, "left support channel.")
-
-        elif before.channel is not None:
-            if before.channel.id == after.channel.id:
-                return
-            if str(before.channel.id) == str(supportchannel) and str(supportchannel) != str(after.channel.id):
-                del tickets[member.id]
-                print(member.name, "left support channel.")
-                
-
-        elif str(after.channel.id) == supportchannel:
+        if after.channel is not None and str(after.channel.id) == supportchannel:
             print(member.name, "joined support channel.")
 
             id = 0
             while id == 0:
                 id = random.getrandbits(64)
                 for item in tickets.keys():
-                    if item["id"] == id:
+                    if tickets[item]["id"] == id:
                         id == 0
 
             tickets[member.id] = {
@@ -112,10 +105,70 @@ class client_class(discord.Client):
                                     "You can use the accept command in the support channel to claim the ticket.\n"
                                     "The ID is: **%s**"
                                     % (str(member.name), str(member.guild), str(id)))
-        
-        print(tickets)
-        
 
+
+            if voice is None: 
+                voice = await channel.connect()
+                with youtube_dl.YoutubeDL({}) as ydl:
+                    song_info = ydl.extract_info(get_setting(member.guild.id, "supportvideo"), download=False)
+                url = song_info["formats"][0]["url"]
+                
+                while True:
+                    if voice.is_connected():
+                        voice.play(discord.FFmpegPCMAudio(url))
+
+                        while voice.is_playing():
+                            await asyncio.sleep(1)
+                    else:
+                        break
+                
+            elif not voice.is_connected():
+                voice = await channel.connect()
+                with youtube_dl.YoutubeDL({}) as ydl:
+                    song_info = ydl.extract_info(get_setting(member.guild.id, "supportvideo"), download=False)
+                url = song_info["formats"][0]["url"]
+
+                while True:
+                    if voice.is_connected():
+                        voice.play(discord.FFmpegPCMAudio(url))
+
+                        while voice.is_playing():
+                            await asyncio.sleep(1)
+                    else:
+                        break
+
+                while voice.is_playing():
+                    await asyncio.sleep(1)
+                await voice.disconnect()
+            else:
+                await voice.move_to(channel)
+
+
+        elif after.channel is None:
+            if str(before.channel.id) == str(supportchannel):
+                try:
+                    del tickets[member.id]
+                except KeyError:
+                    pass
+                if voice is not None and len(tickets.keys()) == 0:
+                    await voice.disconnect()
+                print(member.name, "left support channel.")
+                return
+
+        elif before.channel is not None:
+            if before.channel.id == after.channel.id:
+                return
+            if str(before.channel.id) == str(supportchannel) and str(supportchannel) != str(after.channel.id):
+                try:
+                    del tickets[member.id]
+                except KeyError:
+                    pass
+                if voice is not None and len(tickets.keys()) == 0:
+                    await voice.disconnect()
+                print(member.name, "left support channel.")
+            return           
+        
+        
 globals.init()
 
 from globals import tickets, music
